684851ee8b8afcd32b96263692445d72ad9b83cc
Dhruv <guptadhruv.2001@gmail.com>
2020-10-06 17:22:01 +0530

    fin5

--- server/index.js
+++ server/index.js
@@ -154,4 +154,4 @@ function checkNotAuthenticated(req, res, next) {
   next();
 }
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("Listening on port 3000"));
+server.listen(PORT, () => console.log("Listening on port", PORT));

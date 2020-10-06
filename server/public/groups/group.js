55c21dd319f4f9bdc2bdc56a9a0d13ae03c87fbc
Dhruv <guptadhruv.2001@gmail.com>
2020-10-06 18:45:09 +0530

    indigroup error

--- server/public/groups/group.js
+++ server/public/groups/group.js
@@ -54,5 +54,5 @@ groupList.addEventListener("click", openGroupPage);
 function openGroupPage(e) {
   const groupid = e.target.querySelector(".group-id").innerText;
   sessionStorage.setItem("group", JSON.stringify(groupid));
-  window.location.replace("./indigroup.html");
+  window.location.href = "./indigroup.html";
 }
